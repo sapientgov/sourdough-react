0f83fe862fcc7495b0714db388a7a4d656c6691e
Paul Arce <parce@sapient.com>
2017-11-10 15:20:09 -0500

    adding temp favicon and apple touch icons

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 export default class HomePage extends React.Component {
 
   render() {
     return(
       <div className="container">
-        <h1>Hello</h1>
-        <h2>Is there anybody out there?</h2>
+        <h1>Temp</h1>
+        <h2>Location</h2>
+        <div><Link to="/settings">Settings</Link></div>
+        <div><Link to="/alarm">Alarm</Link></div>
       </div>
     )
   }
-
 }
