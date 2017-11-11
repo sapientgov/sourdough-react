3b057f9b433d30b7e99c0d15334d10dcdb4bd330
Paul Arce <parce@sapient.com>
2017-11-10 20:25:28 -0500

    for display of weather icons

---
+++ src/components/weather-icon-block/weather-icon-block.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class WeatherIconBlock extends React.Component {
+
+  static propTypes = {
+    temp: PropTypes.string,
+    location: PropTypes.string
+  }
+
+  render() {
+    return(
+      <div className="weatherIconBlock">
+        <h1>{this.props.temp}</h1>
+        <h2>{this.props.location}</h2>
+      </div>
+    )
+  }
+
+}
